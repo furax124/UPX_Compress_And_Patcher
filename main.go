7ad165457a16c920b3420d9a05eb7e5af9fdbd68
furax124 <84036582+furax124@users.noreply.github.com>
2025-01-02 16:09:25 +0100

    Update main.go
    some cleanup and refractor of the code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/rand"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -16,32 +17,25 @@ func main() {
 	log.SetFlags(0)
 	filePath := "main.exe"
 
-	// Read the binary file
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatalf("[-] Error reading file: %v", err)
 	}
 
-	// Check if the file is a valid Windows executable
 	if !bytes.Equal(data[:2], []byte{0x4D, 0x5A}) {
 		log.Fatal("[-] It doesn't look like a valid Windows executable.")
 	}
 
 	upxHeader := []byte("UPX!")
 	if !bytes.Contains(data, upxHeader) {
-		// Download and install UPX
-		err := UPX_retrieve.DownloadAndInstallUPX()
-		if err != nil {
+		if err := UPX_retrieve.DownloadAndInstallUPX(); err != nil {
 			log.Fatalf("[-] Error downloading and installing UPX: %v", err)
 		}
 
-		// Compress the file with UPX
-		err = UPX_retrieve.CompressWithUPX(filePath)
-		if err != nil {
+		if err := UPX_retrieve.CompressWithUPX(filePath); err != nil {
 			log.Fatalf("[-] Error compressing file with UPX: %v", err)
 		}
 
-		// Re-read the binary file after compression
 		data, err = ioutil.ReadFile(filePath)
 		if err != nil {
 			log.Fatalf("[-] Error reading file after compression: %v", err)
@@ -54,27 +48,24 @@ func main() {
 
 	log.Println("[*] Sections confusing...")
 
-	
-	randomString := make([]byte, 4)
-	_, err = rand.Read(randomString)
-	if err != nil {
-		log.Fatalf("[-] Error generating random string: %v", err)
+	randomBytes := make([]byte, 4)
+	if _, err = rand.Read(randomBytes); err != nil {
+		log.Fatalf("[-] Error generating random bytes: %v", err)
 	}
 
-	// Patch various sections with the random string
-	patchBytes(data, []byte{0x55, 0x50, 0x58, 0x30, 0x00}, randomString)
-	patchBytes(data, []byte{0x55, 0x50, 0x58, 0x31, 0x00}, randomString)
-	patchBytes(data, []byte{0x55, 0x50, 0x58, 0x32, 0x00}, randomString)
+	patchBytes(data, []byte("[-] This program cannot be run in DOS mode."), []byte(randomString(30)))
+
+	patchBytes(data, []byte{0x55, 0x50, 0x58, 0x30, 0x00}, randomBytes)
+	patchBytes(data, []byte{0x55, 0x50, 0x58, 0x31, 0x00}, randomBytes)
+	patchBytes(data, []byte{0x55, 0x50, 0x58, 0x32, 0x00}, randomBytes)
 
 	log.Println("[*] Version block confusing...")
 
-	// Find and patch the UPX version block
 	offset := bytes.Index(data, upxHeader)
 	if offset != -1 {
 		bytesToReplace := 12 + randomInt(1, 3)
 		randomVersion := make([]byte, bytesToReplace)
-		_, err = rand.Read(randomVersion)
-		if err != nil {
+		if _, err = rand.Read(randomVersion); err != nil {
 			log.Fatalf("[-] Error generating random version: %v", err)
 		}
 		patchBytesByOffset(data, offset-bytesToReplace+4, randomVersion)
@@ -84,36 +75,62 @@ func main() {
 
 	log.Println("[*] Replacing standard DOS Stub message...")
 
-	// Replace DOS Stub message
-	patchBytes(data, []byte("[-] This program cannot be run in DOS mode."), []byte("[+] This program was packed with UPX."))
+	patchBytes(data, []byte("[-] This program cannot be run in DOS mode."), []byte(randomString(30)))
 
 	log.Println("[*] WinAPI changing...")
 
-	// Replace WinAPI function name
 	patchBytes(data, []byte("ExitProcess"), []byte("CopyContext"))
 
 	log.Println("[*] EntryPoint patching...")
+	patchEntryPoint(data)
 
-	// Patch EntryPoint
-	isBuild64 := is64Bit(data)
-	if isBuild64 {
-		patchBytes(data, []byte{0x53, 0x56, 0x57, 0x55}, []byte{0x53, 0x57, 0x56, 0x55})
-	} else {
-		patchBytes(data, []byte{0x00, 0x60, 0xBE}, []byte{0x00, 0x55, 0xBE})
-	}
-
-	// Write the modified binary back to the file
-	err = ioutil.WriteFile(filePath, data, 0644)
-	if err != nil {
+	if err := ioutil.WriteFile(filePath, data, 0644); err != nil {
 		log.Fatalf("[-] Error writing file: %v", err)
 	}
 
 	fmt.Println("[+] Binary patched successfully.")
 }
 
+func patchEntryPoint(data []byte) {
+	if len(data) < 0x40 {
+		log.Fatalf("[-] Invalid file: too small to contain a valid PE header.")
+	}
+	peHeaderOffset := binary.LittleEndian.Uint32(data[0x3C:])
+	if int(peHeaderOffset) >= len(data) {
+		log.Fatalf("[-] PE header offset out of bounds: %d", peHeaderOffset)
+	}
+
+	fmt.Printf("[*] PE Header Offset: 0x%x\n", peHeaderOffset)
+	
+	if !bytes.Equal(data[peHeaderOffset:peHeaderOffset+4], []byte("PE\000\000")) {
+		log.Fatalf("[-] Invalid PE header signature.")
+	}
+	
+	entryPointOffset := peHeaderOffset + 0x28
+	if int(entryPointOffset+4) > len(data) {
+		log.Fatalf("[-] Entry point offset out of bounds: %d", entryPointOffset)
+	}
+
+	entryPointRVA := binary.LittleEndian.Uint32(data[entryPointOffset:])
+	fmt.Printf("[*] Entry Point RVA: 0x%x\n", entryPointRVA)
+	
+	entryPoint := int(entryPointRVA)
+	if entryPoint+4 > len(data) {
+		log.Fatalf("[-] Entry point RVA out of bounds: %d", entryPoint)
+	}
+	
+	if is64Bit(data) {
+		patchBytes(data, []byte{0x53, 0x56, 0x57, 0x55}, []byte{0x53, 0x57, 0x56, 0x55})
+	} else {
+		patchBytes(data, []byte{0x00, 0x60, 0xBE}, []byte{0x00, 0x55, 0xBE})
+	}
+
+	patchedEntryPoint := data[entryPoint : entryPoint+4]
+	fmt.Printf("[+] Patched EntryPoint: %x\n", patchedEntryPoint)
+}
+
 func patchBytes(data []byte, oldBytes, newBytes []byte) {
-	index := bytes.Index(data, oldBytes)
-	if index != -1 {
+	if index := bytes.Index(data, oldBytes); index != -1 {
 		copy(data[index:index+len(newBytes)], newBytes)
 	}
 }
@@ -123,7 +140,6 @@ func patchBytesByOffset(data []byte, offset int, newBytes []byte) {
 }
 
 func is64Bit(data []byte) bool {
-	// Check if the PE file is 64-bit
 	peHeaderOffset := binary.LittleEndian.Uint32(data[0x3C:])
 	machineType := binary.LittleEndian.Uint16(data[peHeaderOffset+4:])
 	return machineType == 0x8664
@@ -135,4 +151,12 @@ func randomInt(min, max int) int {
 		log.Fatalf("[-] Error generating random number: %v", err)
 	}
 	return int(nBig.Int64()) + min
+}
+
+func randomString(length int) string {
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		log.Fatalf("[-] Error generating random string: %v", err)
+	}
+	return hex.EncodeToString(bytes)
 }
